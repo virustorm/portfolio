d2a6b4646454538fc8eb8fd72338a30ba1f6226e
kevin <chengyu0718@hotmail.com>
2020-01-07 14:50:16 -0800

    work page update

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ArrowLeft from '../assets/images/arrow-left-solid.svg';
 import ArrowRight from '../assets/images/arrow-right-solid.svg';
+import iexPic from '../assets/images/iex.png';
+import AerocarPic from '../assets/images/aerocar.png';
 
 export default class mainPage extends Component {
 	constructor(props) {
@@ -30,30 +32,53 @@ export default class mainPage extends Component {
 		if (this.state.page === 1) {
 			return (
 				<div className="mainpage">
-					<img src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
+					<img className="mainpage-arrow" src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
 					<div className="mainpage-centerDiv">
 						<h1>hi, i am kevin</h1>
 						<h1>i am a fullstack developer</h1>
 					</div>
-					<img src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+					<img className="mainpage-arrow" src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+					<div className="mainpage-indicatorDiv">
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+					</div>
 				</div>
 			);
 		} else if (this.state.page === 2) {
 			return (
-				<div className="mainpage">
-					<img src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
-					<div className="mainpage-centerDiv">Browse my work!</div>
-					<img src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+				<div className="mainpage__p2">
+					<img className="mainpage-arrow" src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
+					<h1 className="mainpage__p2-title">Browse my work!</h1>
+					<div className="mainpage__p2-middleDiv">
+						<a href="https://virustorm.github.io/iex/">
+							<img className="mainpage__p2-workImg" src={iexPic} alt="iex" />
+						</a>
+						<a href="https://aerocar.herokuapp.com">
+							<img className="mainpage__p2-workImg" src={AerocarPic} alt="aerocar" />
+						</a>
+					</div>
+					<img className="mainpage-arrow" src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+					<div className="mainpage-indicatorDiv">
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+					</div>
 				</div>
 			);
 		} else if (this.state.page === 3) {
 			return (
 				<div className="mainpage">
-					<img src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
+					<img className="mainpage-arrow" src={ArrowLeft} alt="arrow" onClick={this.arrowLeft} />
 					<div className="mainpage-centerDiv">
 						<h1> Contact Me</h1>
 					</div>
-					<img src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+					<img className="mainpage-arrow" src={ArrowRight} alt="arrow" onClick={this.arrowRight} />
+					<div className="mainpage-indicatorDiv">
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+						<div className="mainpage-indicator" />
+					</div>
 				</div>
 			);
 		}
